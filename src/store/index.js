ec42472bd085d0e68af5ae8ab5637c867d0b7863
Douglas Lopes <douglasdmlopes@gmail.com>
2021-10-06 21:32:14 -0300

    Fix

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,33 @@
-import { createStore } from "vuex";
+import { createStore } from 'vuex'
 
 export default createStore({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
-});
+  state: {
+    todos: []
+  },
+  mutations: {
+    SET_TODOS(state, todos) {
+      state.todos = todos
+    }
+  },
+  actions: {
+    fetchTodos(context) {
+      const todos = [
+        { id: 1, text: 'Estudar HTML & CSS', done: true },
+        { id: 2, text: 'Conceitos Vuex', done: true },
+        { id: 3, text: 'Atomic Design', done: false },
+        { id: 4, text: 'Começar com Nuxt', done: false },
+        { id: 5, text: 'Back-end com Adonis', done: false }
+      ]
+
+      context.commit('SET_TODOS', todos)
+    }
+  },
+  getters: {
+    $allTodos(state) {
+      return state.todos
+    },
+    $doneTodos(state) {
+      return state.todos.filter( (todos) => todos.done)
+    }
+  },
+})
